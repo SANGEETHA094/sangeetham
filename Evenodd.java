9b00e874354197d7a5ca12c04a22a8ecef3da63e
SANGEETHA094 <sangeetha.mece@ksrce.ac.in>
2017-06-23 14:33:52 +0530

    Add files via upload

---
+++ Evenodd.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Evenodd {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		Scanner sd=new Scanner(System.in);
+		i=sd.nextInt();
+         if( i%2==0){
+        	 System.out.println("even");
+         }
+         else
+         {
+        	 System.out.println("odd");
+         }
+        	 
+	}
+
+}
