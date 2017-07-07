3f1556fbacd3745ff9ab3d3a176ced6ba90dc43f
SANGEETHA094 <sangeetha.mece@ksrce.ac.in>
2017-07-07 12:39:31 +0530

    Add files via upload

---
+++ StringToInteger.java
@@ -0,0 +1,12 @@
+package ss;
+
+public class StringToInteger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String number = "10";
+		int result = Integer.parseInt(number);
+		System.out.println(result);
+	}
+
+}
