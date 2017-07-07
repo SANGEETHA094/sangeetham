7dcf9668c87e4f931b2817236f27ed7fd40527b2
SANGEETHA094 <sangeetha.mece@ksrce.ac.in>
2017-07-07 12:35:24 +0530

    Add files via upload

---
+++ IntegertoString.java
@@ -0,0 +1,12 @@
+package ss;
+
+public class IntegertoString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=10;  
+		String s=Integer.toString(i);
+		System.out.println(i);
+	}
+
+}
