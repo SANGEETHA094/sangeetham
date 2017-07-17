e08a9698468fd085ac43f53bf894e15a753eb4df
SANGEETHA094 <sangeetha.mece@ksrce.ac.in>
2017-07-17 11:46:26 +0530

    Add files via upload

---
+++ Power.java
@@ -0,0 +1,28 @@
+package Guvi;
+import java.util.*;
+		public class Power {
+
+		    public static void main(String args[])
+		    {
+		        Scanner sc=new Scanner(System.in);
+		        int number = 0;
+		        int pow = 0;
+		        int power = 0;
+
+		        System.out.print("Enter number: ");
+		        number = sc.nextInt();
+
+		        System.out.print("Enter power: ");
+		        pow = sc.nextInt();
+
+		        System.out.print(power(number,pow));
+		    }
+		    public static int power(int a, int b)
+		    {
+		            int power = 1;
+		            for(int c=0;c<b;c++)
+		            power*=a;
+		            return power;
+		        }
+		}
+		
