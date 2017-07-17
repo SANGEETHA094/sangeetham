33c2445a2be6c69baf467aba52250d6f39b15231
SANGEETHA094 <sangeetha.mece@ksrce.ac.in>
2017-07-17 14:09:08 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,30 @@
+package Guvi;
+import java.util.Collection;
+import java.util.TreeMap;
+
+public class Employee {
+private Collection<TreeMap> child;
+		    private String caption;
+
+  public void TreeNode(Collection<TreeMap> children, String caption) {
+		     this.child=child;
+		     this.caption=caption;}
+  public Collection<TreeMap> getChild() {
+		   return child;}
+		
+
+ public void setChildren(Collection<TreeMap> child) {
+		 this.child=child;}
+		    public String getCaption() {
+		        return caption;
+		    }
+
+		    public void setCaption(String caption) {
+		        this.caption=caption;
+		    }
+
+		}
+
+	
+
+
